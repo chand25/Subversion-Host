fe2e357bc2ed1eac1a16a2a6a21e582d4f933a5a
Chan <moore.shantel@gmail.com>
2017-09-08 15:21:48 -0400

    did merge in Subversion

---
+++ SVNRepoWC/branches/CalculatorSimulator/ConfigManagmentSVG/src/com/SVN/taxes/TaxNotEligibleException.java
@@ -0,0 +1,14 @@
+package com.SVN.taxes;
+
+public class TaxNotEligibleException extends Exception {
+	private static final long serialVersionUID = 1L;
+ 
+	
+	/**
+	 * 
+	 */
+	
+	public TaxNotEligibleException(String message) {
+		super(message);
+	}
+}
