64df55175cc0556f83801e61f5f7fc693d08d645
Chan <moore.shantel@gmail.com>
2017-09-08 13:27:25 -0400

    Adding Tax calculator object
    adding taxCalculator and exception classes

---
+++ SVNRepoWC/Trunk/ConfigManagmentSVG/src/com/SVN/handson/CountryNotValidException.java
@@ -0,0 +1,13 @@
+package com.SVN.handson;
+
+ public class CountryNotValidException extends Exception {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	
+
+	public CountryNotValidException(String message) {
+		super(message);
+	}
+}
---
+++ SVNRepoWC/Trunk/ConfigManagmentSVG/src/com/SVN/handson/EmployeeNameInvalidException.java
@@ -0,0 +1,14 @@
+package com.SVN.handson;
+
+public class EmployeeNameInvalidException extends Exception {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmployeeNameInvalidException(String message) {
+		super(message);
+	}
+}
